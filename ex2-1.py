953bec87a7630e5c6aecbcccb6b62120c416f2ce
Danielly <danym.nunes@gmail.com>
2023-09-12 16:54:19 -0300

    Exercicio 2.1

---
+++ ex2-1.py
@@ -0,0 +1,8 @@
+'''Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+10 + 20 × 30
+4² ÷ 30
+(9⁴ + 2) × 6 - 1'''
+
+10 + 20 * 30
+4**2 / 30
+(9**4 + 2) * 6 - 1
