51051123da738de5288ea4575c2210b08e779168
Danielly <danym.nunes@gmail.com>
2023-09-20 00:25:30 -0300

    Exercicio 3.1

---
+++ ex3-1.py
@@ -0,0 +1,8 @@
+#Complete a tabela a seguir, marcando inteiro ou ponto flutuante dependendo do número apresentado.
+
+print(type(5))
+print(type(5.0))
+print(type(4.3))
+print(type(-2))
+print(type(100))
+print(type(1.333))
