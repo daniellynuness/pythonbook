3e0c62685145638655cbe8e34693e6bc312f028e
Danielly <danym.nunes@gmail.com>
2023-10-30 16:04:45 -0300

    Exercicio 3.2

---
+++ ex3-2.py
@@ -0,0 +1,10 @@
+Complete a tabela a seguir, respondendo True ou False. 
+Considere a = 4, b = 10, c = 5.0, d = 1 e f = 5.
+
+Expressão Resultado        Expressão Resultado
+a == c    ○ False   		b > a     ○ True 
+a < b     ○ True    		c >= f    ○ True 
+d > b     ○ False   		f >= c    ○ True 
+c != f    ○ False   		c <= c    ○ True 
+a == b    ○ False   		c <= f    ○ True 
+c < d     ○ False
