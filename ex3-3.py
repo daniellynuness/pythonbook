2ca3ca5f0a7bf0bac76c98a984f55afce3b81555
Danielly Nunes <107425186+daniellynuness@users.noreply.github.com>
2023-11-27 19:28:40 -0300

    Update ex3-3.py

--- ex3-3.py
+++ ex3-3.py
@@ -1,10 +1,16 @@
 Complete a tabela a seguir utilizando a = True, b = False e c = True.
 
-Expressão Resultado       Expressão Resultado
-a and a   ○ True          a or c    ○ True 
-b and b   ○ False         b or c    ○ True 
-not c     ○ False    		  c or a    ○ True 
-not b     ○ True    		  c or b    ○ True 
-not a     ○ False   		  c or c    ○ True 
-a and b   ○ False   		  b or b    ○ False
+Expressão Resultado      
+a and a   ○ True              
+b and b   ○ False              
+not c     ○ False    		       
+not b     ○ True    		       
+not a     ○ False   		       
+a and b   ○ False   		      
 b and c   ○ False
+a or c    ○ True
+b or c    ○ True
+c or a    ○ True
+c or b    ○ True
+c or c    ○ True
+b or b    ○ False
