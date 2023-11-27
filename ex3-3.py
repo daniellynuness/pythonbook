bc766f27f9ff49d1eb511de79ef7fb2a0c9d4966
Danielly <danym.nunes@gmail.com>
2023-11-27 19:16:49 -0300

    Exercicio 3.3

---
+++ ex3-3.py
@@ -0,0 +1,9 @@
+#Complete a tabela a seguir utilizando a = True, b = False e c = True.
+
+a and a   ○ True              a or c    ○ True 
+b and b   ○ False             b or c    ○ True 
+not c     ○ False    		  c or a    ○ True 
+not b     ○ True    		  c or b    ○ True 
+not a     ○ False   		  c or c    ○ True 
+a and b   ○ False   		  b or b    ○ False
+b and c   ○ False
