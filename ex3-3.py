90fe21b91b570bfa0217cebbf999e79d940bb4b9
Danielly Nunes <107425186+daniellynuness@users.noreply.github.com>
2023-11-27 19:24:08 -0300

    Update ex3-3.py

--- ex3-3.py
+++ ex3-3.py
@@ -1,7 +1,7 @@
 #Complete a tabela a seguir utilizando a = True, b = False e c = True.
 
-a and a   ○ True              a or c    ○ True 
-b and b   ○ False             b or c    ○ True 
+a and a   ○ True          a or c    ○ True 
+b and b   ○ False         b or c    ○ True 
 not c     ○ False    		  c or a    ○ True 
 not b     ○ True    		  c or b    ○ True 
 not a     ○ False   		  c or c    ○ True 
