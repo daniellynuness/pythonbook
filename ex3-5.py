a3515fd7f0ea71dba7059d3f1fbb6cd311193f79
Danielly Nunes <107425186+daniellynuness@users.noreply.github.com>
2024-01-20 22:13:03 -0300

    Exercicio 3-5

---
+++ ex3-5.py
@@ -0,0 +1,16 @@
+''' Calcule o resultado da expressão A > B and C or D, utilizando os valores da tabela a seguir.
+
+A  B C     D      
+1  2 True  False
+10 3 False False
+5  1 True  True'''
+
+Resultados:
+A>B = False and C or D = True 
+False
+
+A>B = True and C or D = False
+False
+
+A>B = True and C or D = True
+True
