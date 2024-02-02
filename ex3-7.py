31109d934b9db00c67c28dd72836cbc604009c58
Danielly Nunes <107425186+daniellynuness@users.noreply.github.com>
2024-02-02 18:44:20 -0300

    Create ex3-7.py

---
+++ ex3-7.py
@@ -0,0 +1,7 @@
+#Faça um programa que peça dois números inteiros. Imprima a soma desses dois números na tela.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+soma = n1+n2
+print(f'{n1}+{n2} = {soma}')
